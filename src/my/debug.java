103c7b2dcdc4124a0f42aa31508e1ea42e84e15e
wangxy <849416854@qq.com>
2021-02-23 14:23:24 +0800

    test

---
+++ src/my/debug.java
@@ -0,0 +1,11 @@
+package my;
+
+public class debug {
+	public static void main (String[] args) {
+		System.out.println("第一行");
+		System.out.println("第二行");
+		System.out.println("第三行");
+	}
+		
+
+}
