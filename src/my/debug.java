3fc7c809e68e3b6c46fa592517a439185da95128
wangxy <849416854@qq.com>
2021-02-23 14:43:13 +0800

    编码

--- src/my/debug.java
+++ src/my/debug.java
@@ -2,7 +2,7 @@ package my;
 
 public class debug {
 	public static void main (String[] args) {
-		System.out.println("一");
+		System.out.println("一66");
 		System.out.println("第二行");
 		System.out.println("123");
 	}
